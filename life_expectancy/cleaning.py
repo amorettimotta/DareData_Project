c9258313981c6826078c48c4d98c3803034c9f26
amorettimotta <amorettimotta@gmail.com>
2023-10-29 19:00:52 +0000

    update cleaning.py

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -37,7 +37,6 @@ def clean_data(country_arg = 'PT'):
     df = df.dropna(subset=['value'])
 
     df['region'] = df['region'].str.extract(r'([A-Z]{2})', expand=False)
-    #df['age'] = df['age'].apply(clean)
 
     df['value'] = df['value'].astype(float)
     df = df[df['region'] == country_arg]
